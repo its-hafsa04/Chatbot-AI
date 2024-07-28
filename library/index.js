aa518cbd350ef117f61205be31c4639ed40d1738
Sunil <sunisimar2002d@gmail.com>
2024-07-28 17:25:38 +0530

    updating index.js

--- library/index.js
+++ library/index.js
@@ -1,14 +1,3 @@
 import ChatAssistant from "./assistant.js";
 
-const chatAssistant = new ChatAssistant();
-
-(async () => {
-  try {
-    const response = await chatAssistant.sendMessage(
-      "what are you doing"
-    );
-    console.log("Response from Gemini API:", response);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-})();
+export {ChatAssistant};
